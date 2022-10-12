56874716fb4ac256b663d7ec28f4a4800a8d1214
kapadokia <ktnyandoch@gmail.com>
2022-10-12 15:33:11 +0300

    transferred business logic to the service

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,25 @@
+import '../css/style.css'
 import React from "react";
 import logo from "../img/logo.png"
-import { Navbar, Nav } from 'rsuite';
-import HomeIcon from '@rsuite/icons/legacy/Home';
-import CogIcon from '@rsuite/icons/legacy/Cog';
 
 export default function NavBar(){
 
     return(
-        <Navbar>
-        <Navbar.Brand href="#"><img src={logo} height={25}/></Navbar.Brand>
-        <Nav>
-          <Nav.Item>Home</Nav.Item>
-          <Nav.Item>Services</Nav.Item>
-          <Nav.Item>Menu</Nav.Item>
-          <Nav.Menu title="About">
-            <Nav.Item>Company</Nav.Item>
-            <Nav.Item>Team</Nav.Item>
-            <Nav.Menu title="Contact">
-              <Nav.Item>Via email</Nav.Item>
-              <Nav.Item>Via telephone</Nav.Item>
-            </Nav.Menu>
-          </Nav.Menu>
-        </Nav>
-        <Nav pullRight>
-          <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
-        </Nav>
-      </Navbar>
+      <header class="header">
+      <a href="#" class="logo"><img src={logo} height='40px'/></a>
+      <nav class="navbar">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#popular">Popular</a>
+          <a href="#menu">Menu</a>
+          <a href="#order">Order</a>
+          <a href="#blogs">Blogs</a>
+      </nav>
+
+      <div class="input-group">
+        <input type="search" class="form-control rounded" placeholder="I Want To Cook..." aria-label="Search" aria-describedby="search-addon" />
+        <button type="button" class="btn btn-outline-primary">search</button>
+      </div>
+  </header>
     )
 }
