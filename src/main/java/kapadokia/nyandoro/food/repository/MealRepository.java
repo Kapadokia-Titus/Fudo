8f99aa83e547dfb948afb23218c2031bc41d64c5
kapadokia <ktnyandoch@gmail.com>
2022-10-15 06:58:08 +0300

    created a method to fetch data

--- src/main/java/kapadokia/nyandoro/food/repository/MealRepository.java
+++ src/main/java/kapadokia/nyandoro/food/repository/MealRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class MealRepository {
@@ -18,12 +19,28 @@ public class MealRepository {
     }
 
     public List<Meal> selectAllMeals(){
-        String sql="";
+        String sql=""+
+                " SELECT meal_id,meal_title, meal_description, meal_image, meal_ingredients, meal_cooking_time " +
+                "FROM meal";
         // return a list of meals from JDBC
         // the jdbcTemplate takes in the sql query, fetches that data and return's to us as a java object format
-        List<Meal> mealList = jdbcTemplate.query(sql, (resultSet, i)->{
-            return null;
+        return jdbcTemplate.query(sql, (resultSet, i)->{
+
+            // get id from the result set
+            String mealIdStr = resultSet.getString("meal_id");
+            // convert the Id to a UUID
+            UUID mealId = UUID.fromString(mealIdStr);
+
+            //grab other details
+            String mealTitle = resultSet.getString("meal_title");
+            String mealDescription = resultSet.getString("meal_description");
+            String mealImage = resultSet.getString("meal_image");
+            String mealIngredients = resultSet.getString("meal_ingredients");
+            String mealCookingTime = resultSet.getString("meal_cooking_time");
+
+            // return the meal data
+            return new Meal(mealId, mealTitle, mealDescription, mealImage, mealIngredients, mealCookingTime);
         });
-      return null;
+
     }
 }
